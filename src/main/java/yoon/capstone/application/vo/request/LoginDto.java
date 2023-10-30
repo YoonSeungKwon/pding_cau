96adea2ba068e3598d37592c11dc58915922df94
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2023-10-30 21:53:20 +0900

    유효성 검증 수정

--- src/main/java/yoon/capstone/application/vo/request/LoginDto.java
+++ src/main/java/yoon/capstone/application/vo/request/LoginDto.java
@@ -3,15 +3,16 @@ package yoon.capstone.application.vo.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import yoon.capstone.application.exception.sequence.ValidationGroup;
 
 @Getter
 public class LoginDto {
 
-    @Email(message = "MEMBER_EMAIL_FORMAT")
-    @NotBlank(message = "MEMBER_EMAIL_BLANK")
+    @Email(message = "MEMBER_EMAIL_FORMAT", groups = ValidationGroup.EmailFormat.class)
+    @NotBlank(message = "MEMBER_EMAIL_BLANK", groups = ValidationGroup.EmailBlank.class)
     private String email;
 
-    @NotBlank(message = "MEMBER_PASSWORD_BLANK")
+    @NotBlank(message = "MEMBER_PASSWORD_BLANK", groups = ValidationGroup.PasswordBlank.class)
     private String password;
 
 }
--- src/main/java/yoon/capstone/application/vo/request/RegisterDto.java
+++ src/main/java/yoon/capstone/application/vo/request/RegisterDto.java
@@ -3,18 +3,19 @@ package yoon.capstone.application.vo.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import yoon.capstone.application.exception.sequence.ValidationGroup;
 
 @Getter
 public class RegisterDto {
 
-    @Email(message = "MEMBER_EMAIL_FORMAT")
-    @NotBlank(message = "MEMBER_EMAIL_BLANK")
+    @Email(message = "MEMBER_EMAIL_FORMAT", groups = ValidationGroup.EmailFormat.class)
+    @NotBlank(message = "MEMBER_EMAIL_BLANK", groups = ValidationGroup.EmailBlank.class)
     private String email;
 
-    @NotBlank(message = "MEMBER_PASSWORD_BLANK")
+    @NotBlank(message = "MEMBER_PASSWORD_BLANK", groups = ValidationGroup.PasswordBlank.class)
     private String password;
 
-    @NotBlank(message = "MEMBER_USERNAME_BLANK")
+    @NotBlank(message = "MEMBER_USERNAME_BLANK", groups = ValidationGroup.NameBlank.class)
     private String name;
 
 
