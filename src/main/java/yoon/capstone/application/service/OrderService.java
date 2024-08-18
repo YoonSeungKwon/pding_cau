280c01e4f9f24693b7322370fbb4a231d6007a06
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-08-18 18:25:07 +0900

    결제 메시지큐, Redis 적용

--- src/main/java/yoon/capstone/application/service/OrderService.java
+++ src/main/java/yoon/capstone/application/service/OrderService.java
@@ -2,9 +2,14 @@ package yoon.capstone.application.service;
 
 import jakarta.persistence.LockTimeoutException;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBucket;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,9 +20,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import yoon.capstone.application.dto.request.OrderDto;
-import yoon.capstone.application.dto.response.KakaoPayResponse;
-import yoon.capstone.application.dto.response.KakaoResultResponse;
-import yoon.capstone.application.dto.response.OrderResponse;
+import yoon.capstone.application.dto.response.*;
 import yoon.capstone.application.entity.*;
 import yoon.capstone.application.enums.ExceptionCode;
 import yoon.capstone.application.exception.OrderException;
@@ -29,10 +32,12 @@ import yoon.capstone.application.repository.ProjectsRepository;
 import yoon.capstone.application.security.JwtAuthentication;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -42,20 +47,29 @@ public class OrderService {
     private String admin_key;
     @Value("${SERVICE_URL}")
     private String serviceUrl;
+    @Value("${RABBITMQ_EXCHANGE_NAME}")
+    private String exchange;
+    @Value("${RABBITMQ_ROUTING_KEY}")
+    private String routingKey;
 
     private final AesBytesEncryptor aesBytesEncryptor;
 
-    private final MemberRepository memberRepository;
-
     private final OrderRepository orderRepository;
 
-    private final PaymentRepository paymentRepository;
-
     private final ProjectsRepository projectsRepository;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private final RedissonClient redissonClient;
 
     private OrderResponse toResponse(Orders orders){
         return new OrderResponse(orders.getMembers().getUsername(), orders.getMembers().getProfile(),
                 orders.getPayment().getCost(), orders.getComments().getContent(), orders.getPayment().getCreatedAt());
+    }
+
+    private ProjectCache toCache(Projects projects){
+        return new ProjectCache(projects.getProjectIdx(), projects.getTitle(), projects.getCurrentAmount(),
+                projects.getGoalAmount(), projects.getParticipantsCount());
     }
 
     @Transactional(readOnly = true)
@@ -83,10 +97,21 @@ public class OrderService {
             throw new UnauthorizedException(ExceptionCode.UNAUTHORIZED_ACCESS); //로그인 되지 않았거나 만료됨
 
         JwtAuthentication memberDto = (JwtAuthentication) authentication.getPrincipal();
-        //Lazy Loading
-        Members currentMember = memberRepository.findMembersByMemberIdx(memberDto.getMemberIdx()).orElseThrow(()->new UnauthorizedException(ExceptionCode.UNAUTHORIZED_ACCESS));
 
-        Projects projects = projectsRepository.findProjectsByProjectIdx(dto.getProjectIdx());
+        //Read Cache Or Cache Warm
+        RBucket<ProjectCache> rBucket = redissonClient.getBucket("projects::" + dto.getProjectIdx());
+        if (rBucket.get() == null) {
+            Projects projects = projectsRepository.findProjectsByProjectIdx(dto.getProjectIdx());
+            rBucket.set(toCache(projects), Duration.ofMinutes(10L));
+        }
+        //
+
+        ProjectCache projects = rBucket.get();
+
+        if(projects.getCurrentAmount() + dto.getTotal() > projects.getGoalAmount())
+            throw new OrderException("목표금액을 초과하였습니다.", HttpStatus.BAD_REQUEST);
+
+
 
         headers.set("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
         headers.set("Authorization", "KakaoAK " + admin_key);
@@ -113,32 +138,44 @@ public class OrderService {
         byte[] encryptByte = aesBytesEncryptor.encrypt(result.getTid().getBytes(StandardCharsets.UTF_8));
         String tid = Base64.getEncoder().encodeToString(encryptByte);
 
-        Payment payment = Payment.builder()
-                .paymentCode(paymentCode)
-                .tid(tid)
-                .cost(dto.getTotal())
-                .build();
+        OrderMessageDto message = new OrderMessageDto(dto.getProjectIdx(), memberDto.getMemberIdx()
+                , dto.getTotal(), dto.getMessage(), tid, paymentCode);
 
-        Comments comments = Comments.builder()
-                .content(dto.getMessage())
-                .build();
+        RBucket<OrderMessageDto> ordersBucket = redissonClient.getBucket("order::" + paymentCode);
 
-        Orders orders = Orders.builder()
-                .projects(projects)
-                .members(currentMember)
-                .payment(payment)
-                .build();
-
-        orders.setComments(comments);
-        orders.setPayment(payment);
-
-        orderRepository.save(orders);
+        ordersBucket.set(message, Duration.ofMinutes(10L));
 
         return result;
     }
 
     @Transactional
     public void kakaoPaymentAccess(String id, String token){
+
+        RBucket<OrderMessageDto> orderBucket = redissonClient.getBucket("order::"+id);
+        OrderMessageDto dto = orderBucket.get();
+
+        //RLock
+        RLock rLock = redissonClient.getLock("projects"+dto.getProjectIdx());
+        try {
+            boolean available = rLock.tryLock(60L, 1L, TimeUnit.SECONDS);
+            if(!available) throw new OrderException(ExceptionCode.ORDER_LOCK_TIMEOUT.getMessage(), ExceptionCode.ORDER_LOCK_TIMEOUT.getStatus());
+
+            RBucket<ProjectCache> projectsRBucket = redissonClient.getBucket("projects::"+dto.getProjectIdx());
+            ProjectCache projects = projectsRBucket.get();
+            if(projects.getGoalAmount() < projects.getCurrentAmount() + dto.getTotal()){
+                throw new OrderException("목표 금액을 초과하였습니다.", HttpStatus.BAD_REQUEST);
+            }else{
+                projects.setCurrentAmount(projects.getCurrentAmount() + dto.getTotal());
+                projects.setParticipantsCount(projects.getParticipantsCount()+1);
+                projectsRBucket.set(projects);
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            throw new OrderException("결제에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }finally {
+            if(rLock.isHeldByCurrentThread())
+                rLock.unlock();
+        }
 
         HttpHeaders headers = new HttpHeaders();
         RestTemplate restTemplate = new RestTemplate();
@@ -147,26 +184,16 @@ public class OrderService {
 
         MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
 
-        Orders orders = orderRepository.findOrdersByPaymentCodeWithFetchJoin(id).orElseThrow(()->new OrderException(ExceptionCode.ORDER_NOT_FOUND.getMessage(),
-                ExceptionCode.ORDER_NOT_FOUND.getStatus()));
+        byte[] byteTid = Base64.getDecoder().decode(dto.getTid());
+        String tid = new String(aesBytesEncryptor.decrypt(byteTid), StandardCharsets.UTF_8);
 
         try {
 
-            Projects projects = projectsRepository.findProjectsByOrderIndexWithLock(orders.getOrderIdx()).orElseThrow(
-                    ()-> new OrderException(ExceptionCode.ORDER_NOT_FOUND.getMessage(), ExceptionCode.ORDER_NOT_FOUND.getStatus())
-            ); //Lock
-
             map.add("cid", "TC0ONETIME");
-            map.add("tid", orders.getPayment().getTid());
-            map.add("partner_order_id", orders.getPayment().getPaymentCode());
-            map.add("partner_user_id", orders.getMembers().getUsername());
+            map.add("tid", tid);
+            map.add("partner_order_id", dto.getPaymentCode());
+            map.add("partner_user_id", dto.getMemberIdx());
             map.add("pg_token", token);
-
-
-            projects.addAmount(orders.getPayment().getCost());
-
-
-            projectsRepository.save(projects);
 
             HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(map, headers); //Lock 처리 후 결제완료 요청
 
@@ -177,13 +204,12 @@ public class OrderService {
             );
 
 
-        }catch (LockTimeoutException e){
-            orderRepository.delete(orders);    //주문 정보 삭제
-            throw new OrderException(ExceptionCode.ORDER_LOCK_TIMEOUT.getMessage(), ExceptionCode.ORDER_LOCK_TIMEOUT.getStatus());
         }catch (Exception e){
             System.out.println(e.getMessage());
-            throw new RuntimeException(e.getMessage());
+            throw new OrderException("결제에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, dto);
     }
 
     @Transactional
