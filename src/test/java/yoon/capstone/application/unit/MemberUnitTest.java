69cb1335d194ca035d8baf8e81ed2bc5e5c9e66e
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-12-16 22:35:02 +0900

    테스트 코드 추가

---
+++ src/test/java/yoon/capstone/application/unit/MemberUnitTest.java
@@ -0,0 +1,38 @@
+package yoon.capstone.application.unit;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberUnitTest {
+
+    /**
+     회원가입 테스트
+     **/
+    @Test
+    void 회원가입_성공_테스트(){
+
+    }
+    @Test
+    void 회원가입_이메일_중복_테스트(){
+
+    }
+    @Test
+    void 회원가입_이메일_체크_테스트(){
+
+    }
+    @Test
+    void 회원가입_AES_인코딩_테스트(){
+
+    }
+
+    /**
+     로그인 테스트
+     **/
+    @Test
+    void 로그인_토큰_테스트(){
+
+    }
+
+
+
+
+}
