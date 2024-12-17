2eb3e033c882764d52ce190b929c7e6fdab90b3a
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-12-17 23:36:00 +0900

    테스트 코드 추가

--- src/test/java/yoon/capstone/application/unit/MemberUnitTest.java
+++ src/test/java/yoon/capstone/application/unit/MemberUnitTest.java
@@ -1,6 +1,10 @@
 package yoon.capstone.application.unit;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import yoon.capstone.application.service.MemberService;
+import yoon.capstone.application.service.domain.Members;
 
 public class MemberUnitTest {
 
@@ -10,6 +14,16 @@ public class MemberUnitTest {
     @Test
     void 회원가입_성공_테스트(){
 
+        MemberService memberService = MemberService.builder()
+                .profileManager()
+                .memberRepository()
+                .aesEncryptorManager()
+                .tokenRefreshTemplate()
+                .build();
+
+
+
+        Assertions.assertEquals();
     }
     @Test
     void 회원가입_이메일_중복_테스트(){
